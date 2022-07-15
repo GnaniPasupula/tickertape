fbc0b18584a0ea0a506a677bfaf588b29ad8e2e0
GnaniPasupula <gnanipasupula@gmail.com>
2022-07-16 00:59:13 +0530

    Added Image links for Card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+const imgs1 = [
+  "https://www.tickertape.in/landingResources/images/carousel/universe.jpg",
+  "https://www.tickertape.in/landingResources/images/carousel/selection.jpg",
+  "https://www.tickertape.in/landingResources/images/carousel/manageScreens.jpg",
+];
+
+const imgs2 = [
+  "https://www.tickertape.in/landingResources/images/carousel/shareholdingpattern.jpg",
+  "https://www.tickertape.in/landingResources/images/carousel/EventsNewsDesktop.jpg",
+  "https://www.tickertape.in/landingResources/images/carousel/EventsNewsDesktop.jpg",
+];
 function Card() {
   return (
     <div className="bg-white mt-10 shadow-2xl">
-      <img src="https://www.tickertape.in/landingResources/images/carousel/universe.jpg" />
+      <img src={imgs1[0]} />
     </div>
   );
 }
