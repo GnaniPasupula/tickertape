82063f93004e8534ea1d361fb9d8c6c2d026632e
GnaniPasupula <gnanipasupula@gmail.com>
2022-07-21 17:20:44 +0530

    Implemented : Stock Popup on clicked Position

--- components/StockPopup.tsx
+++ components/StockPopup.tsx
@@ -7,8 +7,29 @@ import { ArrowsExpandIcon, BookmarkIcon } from "@heroicons/react/outline";
 import React from "react";
 
 function StockPopup(props: any) {
+  var leftp = props.Position;
+  const width = window.innerWidth;
+  var leftf = 0;
+
+  if (leftp + 480 < width - 4) {
+    leftf = leftp;
+  } else {
+    leftf = width - 480 - 4;
+  }
+
+  if (leftf <= 2) {
+    leftf = 16;
+  }
+
+  let lefts = {
+    left: leftf + "px",
+  };
+
   return (
-    <div className="bg-white w-ps h-ph absolute top-9 left-5 rounded-md">
+    <div
+      className={`bg-white w-ps h-ph relative top-9 rounded-md`}
+      style={lefts}
+    >
       <div className="bg-grey h-20 rounded border-b border-grey-dark p-4 flex items-center justify-between">
         <div className="space-y-1">
           <div className="flex items-center space-x-2">
@@ -23,7 +44,7 @@ function StockPopup(props: any) {
           </div>
         </div>
         <div>
-          <ArrowsExpandIcon className="cursor-pointer h-9 w-9 p-2 rounded bg-white border border-grey-dark text-grey-font mr-2" />
+          <ArrowsExpandIcon className="hover:text-blue-500 hover:shadow cursor-pointer h-9 w-9 p-2 rounded bg-white border border-grey-dark text-grey-font mr-2" />
         </div>
       </div>
       <div className="p-4">
@@ -76,12 +97,12 @@ function StockPopup(props: any) {
         </div>
       </div>
       <div className="bg-grey h-16 rounded border-t border-grey-dark p-4 flex items-center justify-between">
-        <BookmarkIcon className="cursor-pointer h-9 w-9 p-2 rounded bg-white border border-grey-dark text-grey-font mr-2" />
-        <button className="h-9 border-t border-grey-dark w-40 flex items-center bg-white">
+        <BookmarkIcon className="hover:shadow cursor-pointer h-9 w-9 p-2 rounded bg-white border border-grey-dark text-grey-font mr-2" />
+        <button className="hover:shadow h-9 border-t border-grey-dark w-40 flex items-center bg-white">
           <ShoppingCartIcon className="h-8 w-8 p-2 rounded bg-transparent text-primary-blue" />
           <p className="text-sm text-primary-blue">Add to Basket</p>
         </button>
-        <button className="h-9 border-t border-grey-dark w-40 flex items-center bg-primary-blue rounded justify-center">
+        <button className="hover:shadow h-9 border-t border-grey-dark w-40 flex items-center bg-primary-blue rounded justify-center">
           <ShoppingCartIcon className="h-8 w-8 p-2 rounded bg-transparent text-white" />
           <p className="text-sm text-white">Place Order</p>
         </button>
--- components/Topscroller.tsx
+++ components/Topscroller.tsx
@@ -3,13 +3,26 @@ import StockPopup from "./StockPopup";
 
 function Topscroller() {
   const [showPopup_stock, setShowPopup_stock] = React.useState(false);
+  const [position, setPosition] = React.useState(0);
+
   return (
     <div
       className="bg-primary-blue h-7 sticky top-0 z-50 rounded border-secondary-blue border-b"
-      onClick={() => setShowPopup_stock(!showPopup_stock)}
+      onClick={(e) => {
+        setShowPopup_stock(!showPopup_stock);
+        setPosition(e.clientX);
+        //window.addEventListener("mousemove", setFromEvent);
+
+        // return () => {
+        //   window.removeEventListener("mousemove", setFromEvent);
+        // };
+      }}
     >
       {showPopup_stock && (
-        <StockPopup setShowPopup_stock={setShowPopup_stock} />
+        <StockPopup
+          //setShowPopup_stock={setShowPopup_stock}
+          Position={position}
+        />
       )}
     </div>
   );
